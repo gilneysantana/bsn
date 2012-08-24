90969c504c9b1da7038040bab184c1a299bdfbd9
Gilney Santana <gilneysantana@gmail.com>
2012-08-24 17:34:51 -0300

    alterei repositorioFActory para core

---
+++ Fontes/core/RepositorioFactory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using bsn.dal;
+
+namespace bsn.core
+{
+    public class RepositorioFactory
+    {
+        public static IQueryable<T> Repositorio<T>(Colecao colecao)
+        {
+            string descricaoColecao = string.Empty;
+
+            switch (colecao)
+            {
+                case Colecao.anuncios:
+                    descricaoColecao = "anuncios";
+                    break;
+                case Colecao.sites:
+                    descricaoColecao = "sites";
+                    break;
+            }
+
+            #region RetornaMock
+            if (descricaoColecao == "sites")
+            {
+                var repositorioSite = new System.Collections.Generic.List<T>();
+
+                switch (typeof(T).ToString())
+                {
+                    case "Site":
+                        repositorioSite.Add(((T)new Site()));
+                        break;
+                    case "Anuncio":
+                        break;
+                }
+
+                //repositorioSite.Add();
+                return repositorioSite.AsQueryable<T>();
+            }
+
+            return null;
+
+            #endregion
+
+            //return new RepositorioMongoDB().Repositorio<T>(descricaoColecao);
+            return new RepositorioMock().Repositorio<T>(descricaoColecao);
+
+        }
+    }
+
+    public enum Colecao
+    {
+        anuncios,
+        sites
+    }
+}
