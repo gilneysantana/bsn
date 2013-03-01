edfee9c1871ec47672f7b4fa8da6111abc5ba798
Gilney Santana <gilneysantana@gmail.com>
2013-02-28 21:23:02 -0300

    faltou isso tudo

--- Fontes/ConsoleApplication1/Program.fs
+++
@@ -1,8 +0,0 @@
-﻿//#r @"C:\projetos\bsn\Fontes\resources\MongoDB.Driver.dll";;
-//#r @"C:\projetos\bsn\Fontes\resources\MongoDB.Bson.dll";;
-//#r @"C:\projetos\bsn\Fontes\core\bin\Debug\core.dll";;
-//
-//
-//let x = bsn.core.TesteLinqPowershell.teste();;
-//x;;
-////let teste = MongoDB.Driver.MongoServer.Create("mongodb://localhost/?safe=true").GetDatabase("bsn").GetCollection<Site>("sites");;
--- Fontes/core/_Script1.fsx
+++
@@ -1,5 +0,0 @@
-﻿#r @"C:\projetos\bsn\Fontes\core\bin\Debug\bsn.core.dll";;
-let x = bsn.core._ConsultasAdhoc.ConsultaGenerica();;
-x;; 
-
-
