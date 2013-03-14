1626d12fecb5570ff3d97320fd4bd8f2d973b0b9
gilneysantana <gilneysantana@gmail.com>
2013-03-14 09:26:48 -0300

    add UtilTest.cs

---
+++ Fontes/bsn.testes/UtilTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using bsn.core.utils;
+
+namespace bsn.testes
+{
+    [TestClass]
+    public class UtilTest
+    {
+        [TestMethod]
+        public void ToCSV_123()
+        {
+            string x = Utils.ToCSV(1,2,3);
+            Assert.AreEqual(@"""1"",""2"",""3""", x);
+        }
+
+        [TestMethod]
+        public void ToCSV_1x3()
+        {
+            string x = Utils.ToCSV(1,"x",3);
+            Assert.AreEqual(@"""1"",""x"",""3""", x);
+        }
+
+        [TestMethod]
+        public void ToCSV_ExemploWikipedia()
+        {
+            string x = Utils.ToCSV(1997,"Ford","E350","Super, \"luxurious\" truck");
+            Assert.AreEqual(@"""1997"",""Ford"",""E350"",""Super, \""luxurious\"" truck""", x);
+        }
+
+        [TestMethod]
+        public void FromCSV_ToCSV_Roundtrip()
+        {
+            var arrOrigem = new string[] { "a", "b", "c" };
+
+            var arrDestino = Utils.FromCSV(Utils.ToCSV(arrOrigem));
+
+            Assert.IsTrue(arrOrigem.SequenceEqual(arrDestino));
+        }
+    }
+}
